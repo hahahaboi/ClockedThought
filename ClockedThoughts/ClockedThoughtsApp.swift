e8a347a02b0e94a3143649d88ff3798bab831877
teja <tejavenkatesh782@gmail.com>
2026-06-19 00:47:41 +0530

    Initial Commit

---
+++ ClockedThoughts/ClockedThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockedThoughtsApp.swift
+//  ClockedThoughts
+//
+//  Created by Lahari on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ClockedThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
